630d6668e8430b019afec2144ceb7466b10726fa
Jerry <haseo.hackers@gmail.com>
2018-04-05 00:06:12 +0900

    [nit] Clean up.

--- app/src/main/java/jerry/githubuserapi/login/model/LoginFormSnapshot.kt
+++ app/src/main/java/jerry/githubuserapi/login/model/LoginFormSnapshot.kt
@@ -20,12 +20,6 @@ data class LoginFormSnapshot(
         return LoginInputValidationResult.OK
     }
 
-    val isEmailValid: Boolean
-        get() = EMAIL_VALIDATION.matches(email)
-
-    val isPasswordValid: Boolean
-        get() = password.isNotEmpty() && password.length > 4
-
     companion object {
         private val EMAIL_VALIDATION: Regex =
             """(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""".toRegex()
